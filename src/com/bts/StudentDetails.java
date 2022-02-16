a30bef46c97b9706e4f93adf9819a40a2f2be9a5
Vinidha19 <vinidhap683@gmail.com>
2022-02-16 08:24:49 -0800

    DemoProject

---
+++ src/com/bts/StudentDetails.java
@@ -0,0 +1,14 @@
+package com.bts;
+
+public class StudentDetails {
+	public void studName() {
+		System.out.println("hardin");
+	}
+	public void studNum() {
+		System.out.println("54321");
+	}
+	public void studId() {
+		System.out.println("C2a");
+	}
+
+}
